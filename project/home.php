0554294c06e398009f2ffa47aa87571373e00ef2
moath3bd <moath3bd@gmail.com>
2024-01-03 18:10:34 +0300

    css

--- project/home.php
+++ project/home.php
@@ -110,6 +110,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['email'], $_POST['pass
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <title>Home Page</title>
 </head>
 <body>
